2ea13aff67d6992b6de5335451af2c4d530dba2e
Sterling Lor <154761732+sarcasmsyl@users.noreply.github.com>
2024-12-07 03:55:38 -0600

    Add files via upload

---
+++ gamedata.py
@@ -0,0 +1,9 @@
+maps_l1 = ['Grasslands', 'Islands', 'Mountains']
+maps_l2 = []
+maps_l3 = []
+
+encounters = ['Monster', 'RandomEvent', 'Boss', 'Heal', 'Shop']
+
+monsters_grasslands = ['Bees', 'Thief', 'Living Rock', 'Evil Grandma', 'Wolf']
+random_events_grasslands = ['Trail of Breadcrumbs', 'Cat in Tree', 'River', 'Traveling Shop', 'Forage']
+bosses_grasslands = ['Werewolf', 'Giant', 'Bigfoot']
